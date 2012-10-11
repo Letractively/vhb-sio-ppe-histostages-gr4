2d4a559837b259f8159d88df22c4a4c8fb2e4569
lagache.yoann@gmail.com <lagache.yoann@gmail.com@a58e2943-9908-b35b-b900-4c01106f1e75>
2012-10-11 21:40:29 +0000

    fixed issue numéro 9.

--- trunk/histostage_seance3/accueil.php
+++ trunk/histostage_seance3/accueil.php
@@ -50,7 +50,7 @@
               <a href="?page=listeOrganisations" title="Liste des organisations ayant accueilli un stagiaire">Liste entreprises</a>
             </li>
             <li>
-              <a href="?page=rechercheStagesCriteres" title="Rechercher un stage sur critères">Recherche stages</a>
+              <a href="?page=rechercheStagesCriteres" title="Rechercher un stage sur critÃ¨res">Recherche stages</a>
             </li>
           </ul> 
          </li>
@@ -62,7 +62,49 @@
 		  
 	
 	 <div id="contenu">
-  <?php if ( isset($_GET["page"]) ) { $page = $_GET["page"]; include('./histo/'.$page.'.php');} ?>
+	 	<h1>
+	 	<?php
+	 	$correspondancePageAvecTitreNiveau1 = 
+	 	array(
+			"accueil" => "<a href='accueil.php'>Historique stages</a>",
+			"competencesSavoirsDA" => " > <a href='accueil.php?page=competencesSavoirsDA'>Compétences savoirs DA</a>",
+			"competencesSavoirsAR" => " > <a href='accueil.php?page=competencesSavoirsAR'>Compétences savoirs AR</a>",
+			"activitesP1" => " > <a href='accueil.php?page=activitesP1'>Activités P1</a>",
+			"activitesP2" => " > <a href='accueil.php?page=activitesP2'>Activités P2</a>",
+			"activitesP3" => " > <a href='accueil.php?page=activitesP3'>Activités P3</a>",
+			"activitesP4" => " > <a href='accueil.php?page=activitesP4'>Activités P4</a>",
+			"activitesP5" => " > <a href='accueil.php?page=activitesP5'>Activités P5</a>",
+			"listeOrganisations" => " > <a href='accueil.php?page=listeOrganisations'>Liste organisations</a>",
+			"rechercheStagesCriteres" => " > <a href='accueil.php?page=rechercheStagesCriteres'>Recherche stages</a>",
+			"adressesSites" => " > <a href='accueil.php?page=adressesSites'>Adresses Sites</a>"	 	
+		);
+		$correspondancePageAvecTitreNiveau2 =
+		array(
+				"detailOrganisation" => $correspondancePageAvecTitreNiveau1["listeOrganisations"]." > <a href='accueil.php?page=detailOrganisation'>Détail d'une organisation</a>",
+				"resultatsRechercheStages" => $correspondancePageAvecTitreNiveau1["rechercheStagesCriteres"]." > <a href='accueil.php?page=resultatsRechercheStages'>Résultat stages</a>"
+		);
+		$correspondancePageAvecTitreNiveau3 =
+		array(
+				"detailStage" => $correspondancePageAvecTitreNiveau2["resultatsRechercheStages"]." > <a href='accueil.php?page=detailStage'>Détail d'un stage</a>"
+		);
+		echo $correspondancePageAvecTitreNiveau1["accueil"];
+		if (isset($_GET["page"])) {
+			if(isset($correspondancePageAvecTitreNiveau1[$_GET["page"]]))
+			{
+				echo $correspondancePageAvecTitreNiveau1[$_GET["page"]];
+			}
+			elseif(isset($correspondancePageAvecTitreNiveau2[$_GET["page"]]))
+			{
+				echo $correspondancePageAvecTitreNiveau2[$_GET["page"]];
+			}
+			elseif(isset($correspondancePageAvecTitreNiveau3[$_GET["page"]]))
+			{
+				echo $correspondancePageAvecTitreNiveau3[$_GET["page"]];
+			}
+		}
+	 	?> 
+	 	</h1>
+  		<?php if ( isset($_GET["page"]) ) { $page = $_GET["page"]; include('./histo/'.$page.'.php');} ?>
 	 </div>
 	
   <!-- Division pour le pied de page -->
