700ff42fecc016ec7d2f17f2ea78d1a7da48b34e
lagache.yoann@gmail.com <lagache.yoann@gmail.com@a58e2943-9908-b35b-b900-4c01106f1e75>
2012-11-13 16:07:39 +0000

    Accueil achevé, reste juste les liens des boutons à définir dedans.

--- branches/uc_ajouterEtudiant/accueil.php
+++ branches/uc_ajouterEtudiant/accueil.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+  <head>
+    <title>Site intranet de la section STS IG-SIO</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link href="styles.css" rel="stylesheet" type="text/css" />
+    <script src="menuCascade.js" type="text/javascript"></script>
+  </head>
+  <body>
     <?php include('histo/includes/entete.html');?>
 
 	<?php include('histo/includes/menuGauche.html');?>
--- branches/uc_ajouterEtudiant/histo/administration/accueilAdmin.php
+++ branches/uc_ajouterEtudiant/histo/administration/accueilAdmin.php
@@ -2,7 +2,51 @@
        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
   <head>
-    <title>Accueil de la zone d'administration</title>
+    <title>Accueil de l'administration</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link href="styles.css" rel="stylesheet" type="text/css" />
+    <link href="../../styles.css" rel="stylesheet" type="text/css" />
     
+    <script src="menuCascade.js" type="text/javascript"></script>
+  </head>
+  <body>
+<?php include('../includes/entete.html');?>
+
+<?php include('../includes/menuGauche.html');?>
+
+ <div id="contenu">
+	<div class="colonneAdmin">
+		<h3>Organisations</h3>
+		<div>
+			<br /><a href=""><input type="button" value="Modifier" /></a><br /><br /><br />
+			<a href=""><input type="button" value="Ajouter" /></a><br />
+		</div>
+	</div>
+	<div class="colonneAdmin">
+		<h3>Contacts</h3>
+		<div>
+			<br /><a href=""><input type="button" value="Modifier" /></a><br /><br /><br />
+			<a href=""><input type="button" value="Ajouter" /></a><br />
+		</div>
+	</div>
+	<div class="colonneAdmin">
+		<h3>Etudiants</h3>
+		<div>
+			<br /><a href="ajoutEtudiant.php"><input type="button" value="Modifier" /></a><br /><br /><br />
+			<a href=""><input type="button" value="Ajouter" /></a><br />
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include('../includes/pied.html');?>
