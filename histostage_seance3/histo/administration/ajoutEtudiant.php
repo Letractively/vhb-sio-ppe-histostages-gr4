36c1745c6e778d06e8007b9f5a10f1a63aa616d4
annie.baraban <annie.baraban@gmail.com>
2012-11-13 17:34:38 +0000

    Page d'accueil administration + Ajout cas d'utilisation ajout étudiant

---
+++ histostage_seance3/histo/administration/ajoutEtudiant.php
@@ -0,0 +1,49 @@
+<html>
+        <head>
+                <title>Ajout des étudiants</title>
+                <script type="text/javascript">
+                function redirection(s, url)
+                {
+                        ms = s * 1000; // conversion des secondes en microseconde
+                        self.setTimeout("self.location.href = '" + url + "';", ms) ;
+                }
+                function ecrireTexteRedirection(temps) {
+                        var p = document.getElementById('paragraphe');                  
+                        p.innerHTML=" Vous serez automatiquement redirigé vers le formulaire dans "+ temps + " secondes.";
+                        console.log(temps);
+                }
+                </script>
+        </head>
+<?php 
+include ('../histo/_gestionBase.inc.php');
+
+if (move_uploaded_file($_FILES["caseUpload"]["tmp_name"], "etudiant.csv")) {
+        $uploaded=true;
+        $connexion=connect();
+        selectBase($connexion);
+        $fichier = "etudiant.csv";
+        $fic = fopen($fichier, 'r');
+        $indices= array("nom","prenom","libOption");
+        $ligne=array();
+        if ($fic !== FALSE) {
+                while(($data = fgetcsv($fic, 1000, ";")) !== false) {
+                        for ($i = 0; $i < 3; $i++) {
+                                $ligne[$indices[$i]] = $data[$i];
+                        }
+                        insertEtudFromCSV($ligne["nom"],$ligne["prenom"],$ligne["libOption"],$connexion);
+                }
+                fclose($fic);
+                unlink($fichier);//supprime le fichier uploadé après avoir fait les actions
+        }
+}
+else {
+        $uploaded=false;
+}?>
+        <body onload="redirection(5, 'formAjoutEtudiant.php');">
+                <p id="paragraphe"><?php $msg=$uploaded?"Etudiants ajoutés":"Echec de l'ajout des étudiants"; echo $msg ?></p>
+                <script type="text/javascript">
+                var unTemps = 4;                
+                timer = setInterval("ecrireTexteRedirection(unTemps);unTemps--", 1000);
+                </script>
+        </body>
+</html>
