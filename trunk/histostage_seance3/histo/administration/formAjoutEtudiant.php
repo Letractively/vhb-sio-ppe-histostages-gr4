e09e8931065184f3a26f175954b35cd94c3d6366
lagache.yoann@gmail.com <lagache.yoann@gmail.com@a58e2943-9908-b35b-b900-4c01106f1e75>
2012-11-20 14:51:17 +0000

    Ajout bouton retour au formulaire d'ajout étudiant. Intégration de cette page avec le design du site.

--- trunk/histostage_seance3/histo/administration/formAjoutEtudiant.php
+++ trunk/histostage_seance3/histo/administration/formAjoutEtudiant.php
@@ -1,12 +1,23 @@
-<html>
-<title>Ajouter un étudiant</title>
-<body>
-        <section>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+  <head>
+    <title>Ajouter un étudiant</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link href="../../styles.css" rel="stylesheet" type="text/css" />
+    
+    <script src="menuCascade.js" type="text/javascript"></script>
+  </head>
+  <body>
+<?php include('../includes/entete.html');?>
+
+<?php include('../includes/menuGauche.html');?>
+        <div id="contenu">
                 <form enctype="multipart/form-data" action="ajoutEtudiant.php" method="post">
                         <label for="caseUpload">Fichier à importer</label>
                         <input type="file" name="caseUpload" id="caseUpload" /><br />
                         <input type="submit" id="btnEnvoyer" value="Importer"/>
                 </form>
-        </section>
-</body>
-</html>
+                <a href="accueilAdmin.php">Retour à l'accueil de l'administration</a>
+        </div>
+<?php include('../includes/pied.html');?>
